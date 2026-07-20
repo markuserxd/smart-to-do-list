90eaa15494cb3753674feb7675e96ee179eb50b8
markuserxd <thanawatkulapun@gmail.com>
2026-07-20 20:54:40 +0700

    add validation for create task

--- server/controllers/taskController.js
+++ server/controllers/taskController.js
@@ -7,6 +7,23 @@ exports.getAllTasks = (req, res) => {
 };
 
 exports.createTask = (req, res) => {
+
+    const { title, priority } = req.body;
+
+    if (!title || title.trim() === "") {
+        return res.status(400).json({
+            message: "Title is required"
+        });
+    }
+
+    const priorities = ["Low", "Medium", "High"];
+
+    if (priority && !priorities.includes(priority)) {
+        return res.status(400).json({
+            message: "Invalid priority"
+        });
+    }
+
     const task = taskModel.createTask(req.body);
 
     res.status(201).json(task);
